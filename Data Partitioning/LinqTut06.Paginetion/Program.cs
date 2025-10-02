93b3651a8ee7a8cf06b8666441e296a958e8cee1
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-02 19:07:58 +0300

    Adding example

--- Data Partitioning/LinqTut06.Paginetion/Program.cs
+++ Data Partitioning/LinqTut06.Paginetion/Program.cs
@@ -12,9 +12,18 @@ namespace LinqTut06.Paginetion
             var emps = Repository.LoadEmployees();
 
             var page = 1;
-            var size = 20;
-            var result   = emps.Paginate(page,size);
-            result.Print($"Employees Page {page}");
+            var size = 10;
+
+
+            var q1   = emps.Paginate(page,size);
+            q1.Print($"Employees Page {page}");
+
+
+            page = 3;
+            size = 10;
+
+            var q2 = emps.Paginate(page,size);
+            q2.Print($"Employees Page {page}");
             
             
 
