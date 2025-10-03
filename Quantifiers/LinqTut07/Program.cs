9a4b42967cbae787b544115ad1a93429b7597d36
Abbas Aqeel <abbas.aqeel.ismael@gmail.com>
2025-10-04 01:12:44 +0300

    Adding some Comments

--- Quantifiers/LinqTut07/Program.cs
+++ Quantifiers/LinqTut07/Program.cs
@@ -21,13 +21,16 @@ namespace LinqTut07
         {
 
             var employees = Repository.LoadEmployees();
-
+            
+            //this will search if there is any employee has jac in his name?
             var q1 = employees.Any(x => x.Name.StartsWith("jac", StringComparison.OrdinalIgnoreCase));
             Console.WriteLine($"Check if there is an employee with name starts with jac: {q1}");
 
+            //this will search if there is any employee has Salary below 10,000
             var q2 = employees.Any(x => x.Salary < 10000);
             Console.WriteLine($"Check if there is an employee with salary less than 10.000: {q2}");
 
+            //this will search if there is any employee has one skill?
             var q3 = employees.Any(x => x.Skills.Count == 1);
             Console.WriteLine($"Check if there is an employee have one Skill?: {q3}");
 
@@ -39,10 +42,11 @@ namespace LinqTut07
 
             var employees = Repository.LoadEmployees();
 
+            //True if all employees have emails , false if there is one employee has no email
             var q1 = employees.All(x => !string.IsNullOrWhiteSpace(x.Email));
             Console.WriteLine($"If all Employees have Emails?: {q1}");
 
-
+            //False if there is one employee has no C# skill
             var q2 = employees.All(x => x.Skills.Any(s => s =="C#"));
             Console.WriteLine($"If all Employees have C# Skill?: {q2}");
 
@@ -55,6 +59,7 @@ namespace LinqTut07
 
             var employees = Repository.LoadEmployees();
 
+            //Select employees that have C++ skill
             var q1 = from employee in employees
                      where employee.Skills.Any(s => s == "C++")
                      select employee;
@@ -62,7 +67,7 @@ namespace LinqTut07
             q1.Print("All employees with C++ Skill:");
 
 
-
+            //this will not include employees with C# skill
             var q2 = from employee in employees
                      where employee.Skills.All(s => s.Length >= 3)
 
@@ -83,7 +88,7 @@ namespace LinqTut07
             Console.WriteLine($"check if any employee has ee in his name: {result1}");
 
             var e1 = new Employee { Email = "Evans.Hester@example.com" };
-            
+            //This will check if there is an employee with specific Email
             var result2 = employees.Contains(e1);
             Console.WriteLine($"check if any employee has emial with Evans.Hester@example.com: {result2}");
             
