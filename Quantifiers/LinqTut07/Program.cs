a4fc1b013e8f5c69ba167dd2a2287f5bb42d7743
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-04 00:08:44 +0300

    Lesson 07

---
+++ Quantifiers/LinqTut07/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using LINQTut07.Shared;
+
+
+namespace LinqTut07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //RunAnyDemo();
+            //RunAllDemo();
+            //RunAllDemoQuerySyntax();
+            RunContainsDemo();
+
+            Console.ReadKey();
+        }
+
+        private static void RunAnyDemo()
+        {
+
+            var employees = Repository.LoadEmployees();
+
+            var q1 = employees.Any(x => x.Name.StartsWith("jac", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine($"Check if there is an employee with name starts with jac: {q1}");
+
+            var q2 = employees.Any(x => x.Salary < 10000);
+            Console.WriteLine($"Check if there is an employee with salary less than 10.000: {q2}");
+
+            var q3 = employees.Any(x => x.Skills.Count == 1);
+            Console.WriteLine($"Check if there is an employee have one Skill?: {q3}");
+
+
+        }
+
+        private static void RunAllDemo()
+        {
+
+            var employees = Repository.LoadEmployees();
+
+            var q1 = employees.All(x => !string.IsNullOrWhiteSpace(x.Email));
+            Console.WriteLine($"If all Employees have Emails?: {q1}");
+
+
+            var q2 = employees.All(x => x.Skills.Any(s => s =="C#"));
+            Console.WriteLine($"If all Employees have C# Skill?: {q2}");
+
+
+
+        }
+
+        private static void RunAllDemoQuerySyntax()
+        {
+
+            var employees = Repository.LoadEmployees();
+
+            var q1 = from employee in employees
+                     where employee.Skills.Any(s => s == "C++")
+                     select employee;
+            
+            q1.Print("All employees with C++ Skill:");
+
+
+
+            var q2 = from employee in employees
+                     where employee.Skills.All(s => s.Length >= 3)
+
+                     select employee;
+
+            q2.Print("Employees with skills have 3 char and more.");
+
+
+        }
+
+        private static void RunContainsDemo()
+        {
+
+            var employees = Repository.LoadEmployees();
+
+            var result1 = employees.Any(x => x.Name.Contains("ee"));
+
+            Console.WriteLine($"check if any employee has ee in his name: {result1}");
+
+            var e1 = new Employee { Email = "Evans.Hester@example.com" };
+            
+            var result2 = employees.Contains(e1);
+            Console.WriteLine($"check if any employee has emial with Evans.Hester@example.com: {result2}");
+            
+        }
+
+    }
+}
