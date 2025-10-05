87f4d0c8707fbd1f85bf4dc02fa18363055ec06c
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-06 00:54:41 +0300

    Adding lesson 10 files

---
+++ Generation Operations/LinqTut10.DefaultIfEmpty/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Shared;
+
+
+namespace LinqTut10.DefaultIfEmpty
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var questions1 = Enumerable.Empty<Question>();
+
+            var questions2 = questions1.DefaultIfEmpty();
+
+            var questions3 = questions2.DefaultIfEmpty(Question.Default);
+
+
+
+
+
+
+
+            //var range = Enumerable.Range(0, 10);
+
+            //var questions4 = QuestionBank.GetQuestionRange(range);
+
+            //foreach (var questions in questions4)
+            //{
+
+            //    Console.WriteLine(questions);
+
+            //}
+
+            var q = QuestionBank.PickOne();
+
+            var questionss = Enumerable.Repeat(q, 20);
+
+            questionss.ToQuiz();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Generation Operations/LinqTut10.Empty/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Shared;
+
+
+namespace LinqTut10.Empty
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            //var questions = new List<Question>();
+
+            //foreach (var question in questions)
+            //{
+            //    Console.WriteLine(question);
+            //}
+            
+
+            var questions2 = Enumerable.Empty<Question>();
+
+            foreach (var question in questions2)
+            {
+                Console.WriteLine(question);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
