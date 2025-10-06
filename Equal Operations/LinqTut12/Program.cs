832e7cdbce8025a2218c65f9ac5d9c92c0c5d94b
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-06 11:57:18 +0300

    Lesson 12

---
+++ Equal Operations/LinqTut12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqTut12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
