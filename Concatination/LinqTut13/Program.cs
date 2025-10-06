8bd0025aa55809d9d71fb4dcaec2e1a1d04156cb
Abbas Aqeel <abbas.aqeel.ismael@gmail.com>
2025-10-06 14:42:03 +0300

    Update Program.cs

--- Concatination/LinqTut13/Program.cs
+++ Concatination/LinqTut13/Program.cs
@@ -21,7 +21,9 @@ namespace LinqTut13
 
 
             var QuestionTitle = q1.Concat(q2);
-
+            Console.WriteLine("\n\n\n------------------------------------------------------------");
+            Console.WriteLine("Run Method 01 simple Concaenation");
+            Console.WriteLine("------------------------------------------------------------\n");
             foreach(var title in  QuestionTitle)
             {
                 Console.WriteLine(title);
@@ -35,7 +37,9 @@ namespace LinqTut13
 
 
             var QuestionTitle = q1.Select(q => q.Title).Concat(q2.Select(q => q.Title));
-
+            Console.WriteLine("\n\n\n------------------------------------------------------------");
+            Console.WriteLine("Run Method 02 Concatenate specific properties");
+            Console.WriteLine("------------------------------------------------------------\n");
             foreach (var title in QuestionTitle)
             {
                 Console.WriteLine(title);
@@ -47,11 +51,13 @@ namespace LinqTut13
             var q1 = QuestionBank.Randomize(3);
             var q2 = QuestionBank.Randomize(5);
             var q3 = QuestionBank.Randomize(5);
-
+        
             var QuestionTitle = q1.Select(q => q.Title).
                 Concat(q2.Select(q => q.Title)).
                 Concat(q3.Select(q => q.Title));
-
+            Console.WriteLine("\n\n\n------------------------------------------------------------");
+            Console.WriteLine("Run Method 03 more than two concatinations");
+            Console.WriteLine("------------------------------------------------------------\n");
             foreach (var title in QuestionTitle)
             {
                 Console.WriteLine(title);
@@ -63,7 +69,9 @@ namespace LinqTut13
             var q1 = QuestionBank.Randomize(3);
             var q2 = QuestionBank.Randomize(5);
             var q3 = QuestionBank.Randomize(5);
-            
+            Console.WriteLine("\n\n\n------------------------------------------------------------");
+            Console.WriteLine("Run Method 04 Concatination using instantiation");
+            Console.WriteLine("------------------------------------------------------------\n");
             var questions1 = new[] { q1, q2, q3 }.SelectMany(q => q);
             questions1.ToQuiz();
             
