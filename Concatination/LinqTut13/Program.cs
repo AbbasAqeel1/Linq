06563da3e6240f300b1b019e8c43b09683d032a7
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-06 14:03:00 +0300

    Lesson 13 Project

---
+++ Concatination/LinqTut13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LinqTut13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+
+    }
+}
