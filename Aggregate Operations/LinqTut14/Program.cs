06fe1554d5a625508c8bc2c80b98a1cf1110ef56
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-08 02:44:16 +0300

    Adding some examples

--- Aggregate Operations/LinqTut14/Program.cs
+++ Aggregate Operations/LinqTut14/Program.cs
@@ -12,14 +12,14 @@ namespace LinqTut10
         public static void Main(string[] args)
         {
 
-            RunMethod01();
-            RunMethod02();
-            RunMethod03();
-            RunCount();
-            RunMaxQuestionTitleLength();
-            RunMaxByQuestionTitleLength();
-            RunMinQuestionTitleLength();
-            RunMinByQuestionTitleLength();
+            //RunMethod01();
+            //RunMethod02();
+            //RunMethod03();
+            //RunCount();
+            //RunMaxQuestionTitleLength();
+            //RunMaxByQuestionTitleLength();
+            //RunMinQuestionTitleLength();
+            //RunMinByQuestionTitleLength();
             RunSum();
             RunAverage();
             Console.ReadKey();
@@ -35,16 +35,18 @@ namespace LinqTut10
                 return $"{a},{b}";
             });
 
-            Console.WriteLine(result);
+            Console.WriteLine($"The result of concatination: result");
         }
 
         private static void RunMethod02()
         {
-            var names = new[] { 1, 2, 3, 4, 5 };
+            var nums = new[] { 1, 2, 3, 4, 5 };
 
-            var result = names.Aggregate(100,(a, b) => a + b);
-
-            Console.WriteLine($"{result}");
+            var result = nums.Aggregate(100,(a, b) => a + b);
+            var Multiplication = nums.Aggregate(1, (a, b) => a * b);
+            
+            Console.WriteLine($"The result of Sum: {result}");
+            Console.WriteLine($"The result of Multiply: {Multiplication}");
         }
 
 
@@ -54,13 +56,22 @@ namespace LinqTut10
 
             var longestQuestionTitle = quiz[0];
 
-            Console.WriteLine($"{longestQuestionTitle}\n\n");
-
+            
             var result = quiz.Aggregate(longestQuestionTitle,
                                           (longest, next) => longest.Title.Length < next.Title.Length ? next : longest,
                                           (x => x));
+            
+            Console.WriteLine($"Longest Question Title: {result}");
 
-            Console.WriteLine($"{result}");
+
+            var ShortestQuestionTitle = quiz[0];
+
+
+            var result2 = quiz.Aggregate(ShortestQuestionTitle,
+                              (shortest, next) => shortest.Title.Length > next.Title.Length ? next : shortest,
+                              (x => x));
+
+            Console.WriteLine($"Shortest Question Title: {result2}");
         }
 
 
@@ -68,8 +79,9 @@ namespace LinqTut10
         {
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
-            Console.WriteLine(questions.Count());
-            Console.WriteLine(questions.Count(x => x.Title.Length > 150));
+            Console.WriteLine($"Number of questions: {questions.Count()}");
+            Console.WriteLine($"Number of questions that length of the title is greater than 150: " +
+                $"{questions.Count(x => x.Title.Length > 150)}");
             Console.WriteLine(questions.Where(x => x.Title.Length > 150).Count());
         }
         private static void RunMaxQuestionTitleLength()
@@ -77,43 +89,48 @@ namespace LinqTut10
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
             Console.WriteLine($"Max Question Title Length is: {questions.Max(x => x.Title.Length)}");
+            Console.WriteLine($"Max choices in question is: {questions.Max(x => x.Choices.Count)}");
         }
 
         private static void RunMaxByQuestionTitleLength()
         {
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
-            Console.WriteLine($"{questions.MaxBy(x => x.Title.Length)}");
+            Console.WriteLine($"Max Question title length: \n\n{questions.MaxBy(x => x.Title.Length)}");
+            Console.WriteLine($"Question that has max choices: \n\n{questions.MaxBy(x => x.Choices.Count)}");
+
         }
 
         private static void RunMinQuestionTitleLength()
         {
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
-            Console.WriteLine($"{questions.Min(x => x.Title.Length)}");
+            Console.WriteLine($"Minimum question Title length is: {questions.Min(x => x.Title.Length)}");
         }
 
 
 
         private static void RunMinByQuestionTitleLength()
+
         {
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
-            Console.WriteLine($"{questions.MinBy(x => x.Title.Length)}");
+            Console.WriteLine($"Question that has Minimum title length: {questions.MinBy(x => x.Title.Length)}");
         }
 
         private static void RunSum()
         {
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
-            Console.WriteLine($"{questions.Sum(x => x.Choices.Count)}");
+            Console.WriteLine($"Sum of all choices: {questions.Sum(x => x.Choices.Count)}");
+           
         }
 
         private static void RunAverage()
         {
             var questions = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
 
-            Console.WriteLine($"{questions.Average(x => x.Choices.Count)}");
+            Console.WriteLine($"Average of Choices: {questions.Average(x => x.Choices.Count)}");
         }
     }
 
