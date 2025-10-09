6a22b46bcc1f36847631013d4d4539d4e4d8a581
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-09 21:06:06 +0300

    Adding lesson 17 files

---
+++ IEnumerable Vs. IQueryable/LinqTut17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace LinqTut17
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
