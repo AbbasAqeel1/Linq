4f9eb594da7ff046a34c2e0c6481a98a8fe68054
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-09 21:00:36 +0300

    Add Examples

--- Expression Trees/LinqTut16/Program.cs
+++ Expression Trees/LinqTut16/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace LinqTut16
 {
@@ -6,8 +7,18 @@ namespace LinqTut16
     {
 
         public static void Main(string[] args)
-        {
+        { 
+            Func<int,bool> IsEven = num => num % 2 == 0;
 
+            var num = 10;
+            Console.WriteLine($"Is {num} even num? => ({IsEven(num)})");
+            Console.WriteLine(IsEven.Invoke(num).ToString());  
+
+
+            Expression<Func<int,bool>> isEvenExpression = num => num % 2 == 0;
+            Func<int,bool> isEvenV2 = isEvenExpression.Compile();
+
+            Console.WriteLine(isEvenV2(10));
 
             Console.ReadKey();
         }
