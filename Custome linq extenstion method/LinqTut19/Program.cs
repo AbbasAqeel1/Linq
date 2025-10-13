a2e5f9aadb43805b37b00eba40d4fae0dd7a1e3f
Abbas AQEEL <AbbasAQEEL@gmail.com>
2025-10-13 11:47:18 +0300

    Adding Lesson 19 files

---
+++ Custome linq extenstion method/LinqTut19/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Shared;
+
+
+namespace LinqTut19
+{
+
+    internal class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            var Employees = Repository.Employees;
+
+            var Page = 4;
+            var Pages = 10;
+
+            //var Page4X10 = Employees.Paginate(Page, Pages);
+            //Page4X10.Print("Employees Page (4) [10 Employees]");
+
+            //var Page1X10 = Employees.Paginate();
+            //Page1X10.Print("Employees Page (1) [10 Employees]");
+
+            //var Page1X7 = Employees.Paginate2(null, 7);
+            //Page1X7.Print("Employees Page 1 [7 Employees]");
+
+            var EmployeesWithPensionPlan = Employees.WhereWithPaginate(x => x.HasPensionPlan, 1, 5);
+            EmployeesWithPensionPlan.Print("Employees With pension plan #Page 1 [5 Employees]");
+
+            var EmployeesWithoutPensionPlan = Employees.WhereWithPaginate(x => !x.HasPensionPlan, 1, 5);
+            EmployeesWithoutPensionPlan.Print("Employees Without pension plan #Page 1 [5 Employees]");
+
+
+            var EmployeesWithHealthInsurance = Employees.WhereWithPaginate(x => x.HasHealthInsurance, 1, 5);
+            EmployeesWithHealthInsurance.Print("Employees With Health Insurance #Page 1 [5 Employees]");
+
+            var EmployeesWithoutHealthInsurance = Employees.WhereWithPaginate(x => !x.HasHealthInsurance, 1, 5);
+            EmployeesWithoutHealthInsurance.Print("Employees Without Health Insurance #Page 1 [5 Employees]");
+
+            var RandomEmployee = Employees.Random(x => true);
+            Console.WriteLine("\n\nRandom Employee");
+            Console.WriteLine(RandomEmployee);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Custome linq extenstion method/Shared/Extensions.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Shared
+{
+    public static class Extensions
+    { 
+        private static Random _random = new Random();
+
+        public static void Print<T>(this IEnumerable<T> source, string title)
+        {
+            if (source == null)
+                return;
+            Console.WriteLine();
+            Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+            Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+            Console.WriteLine("└───────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+            foreach (var item in source)
+            {
+                if (typeof(T).IsValueType)
+                    Console.Write($" {item} "); // 1, 2, 3
+                else
+                    Console.WriteLine(item);
+            } 
+        }
+
+        public static IEnumerable<Tsource> Paginate<Tsource>(this IEnumerable<Tsource> source,
+            int Page = 1, int PageSize = 10)
+        {
+
+            if (source == null)
+                throw new ArgumentException($"{nameof(source)}");
+      
+            if (Page <= 0)
+                //Page = 1;
+                throw new ArgumentException($"{nameof(Page)}");
+
+            if (PageSize <= 0)
+                //PageSize = 10;
+                throw new ArgumentException($"{nameof(PageSize)}");
+
+            if (!source.Any())
+                return Enumerable.Empty<Tsource>();
+
+            return source.Skip((Page -1 ) * PageSize).Take(PageSize);
+        }
+
+        public static IEnumerable<Tsource> Paginate2<Tsource>(this IEnumerable<Tsource> source,
+            int? Page, int? PageSize)
+        {
+
+            if (source == null)
+            {
+
+                throw new ArgumentException($"{nameof(source)}");
+            }
+
+            if (!Page.HasValue)
+                Page = 1;
+
+            if (!PageSize.HasValue)
+                PageSize = 10;
+
+            if (!source.Any())
+            {
+                return Enumerable.Empty<Tsource>();
+            }
+
+            return source.Skip((Page.Value - 1) * PageSize.Value).Take(PageSize.Value);
+        }
+
+        public static IEnumerable<Tsource> WhereWithPaginate<Tsource>(this IEnumerable<Tsource> source,
+            Func<Tsource, bool> predicate
+            ,int Page, int PageSize)
+        {
+
+            if (source == null)
+            {
+                throw new ArgumentException($"{nameof(source)}");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentException($"{nameof(predicate)}");
+            }
+
+
+            var result = Enumerable.Where(source, predicate);
+           
+
+            return Paginate2(result,Page, PageSize);
+        }
+
+        public static Tsource Random<Tsource>(this IEnumerable<Tsource> source,
+            Func<Tsource, bool> predicate)
+        {   
+
+            if (source == null)
+            {
+                throw new ArgumentException($"{nameof(source)}");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentException($"{nameof(predicate)}");
+            }
+
+            if (!source.Any())
+                return default;
+
+            var result = Enumerable.Where(source, predicate);
+
+            return result.ElementAt(_random.Next(1, source.Count()));
+
+             
+            
+
+            
+
+
+        }
+    }
+}
